7b822832605bbcbb692aceae3b09ea7fb99b36bb
edavogustto <ernesto.daogustto@gmail.com>
2022-01-17 17:25:09 -0300

    Estructura html, jinja

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return "Hello world Flask"
+    user_ip = request.cookies.get('user_ip')
+
+    return render_template('hello.html', user_ip=user_ip)
