7e607bb003eab77fb80100aadded6f3f50227b15
edavogustto <ernesto.daogustto@gmail.com>
2022-01-18 22:10:29 -0300

    Estructurando app

--- main.py
+++ main.py
@@ -2,21 +2,9 @@ from ensurepip import bootstrap
 from wsgiref.validate import validator
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from app import create_app
 
-
-
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de Usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+app = create_app()
 
 @app.errorhandler(404)
 def not_found(error):
@@ -32,28 +20,15 @@ def index():
 
     return response
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
-        'login_form': login_form,
         'username' : username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usuario registrado con éxito')
-
-        return redirect(url_for('hello'))
-
-
-
 
     return render_template('hello.html', **context)
 
