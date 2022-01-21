04b21b92a5b8ec2d8bf5262f115957357212fbac
edavogustto <ernesto.daogustto@gmail.com>
2022-01-20 22:55:09 -0300

    Creando metodo de registro

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,10 @@
 from crypt import methods
+from multiprocessing import context
+from app.firestore_service import get_user, user_put
+from app.models import UserData, UserModel
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.forms import LoginForm
 
@@ -15,10 +20,68 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Nombre de usuario registrado con éxito')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if check_password_hash(user_doc.to_dict()['password'], password_from_db):
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo!')
+
+                redirect(url_for('hello'))
+            else:
+                flash('Alguno de los datos es incorrecto')
+        else: 
+            flash('El usuario no existe')
 
         return redirect(url_for('hello'))
 
     return render_template('login.html', **context)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form' : signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Bienvenido')
+
+            return redirect(url_for('hello'))
+
+        else:
+            flash('El usuario ya existe')
+
+    return render_template('signup.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa Pronto!')
+
+    return redirect(url_for('auth.login'))
